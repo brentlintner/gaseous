692021833a5fe9d72a9b0f5296d580d81c52a5b1
brent lintner <brent.lintner@gmail.com>
2011-01-31 17:51:39 -0500

    tweaked example a bit

--- example/client.js
+++ example/client.js
@@ -1,15 +1,13 @@
-window.addEventListener("load", function (event) {
-    gaseous.client.listen("127.0.0.1", 8888, function (lib) {
-        lib.fs.readFile("README.md", "utf-8", function (err, data) {
-            document.querySelector('body').innerHTML = "<pre>" + data + "</pre>";
-        });
+gaseous.client.listen("127.0.0.1", 8888, function (client) {
+    client.fs.readFile("README.md", "utf-8", function (err, data) {
+        document.querySelector('body').innerHTML = "<pre>" + data + "</pre>";
+    });
 
-        lib.fs.writeFile("TEST.md", "HELLO WORLD!", "utf-8", function (err) {
-            if (err) {
-                throw err;
-            }
+    client.fs.writeFile("TEST.md", "HELLO WORLD!", "utf-8", function (err) {
+        if (err) {
+            throw err;
+        }
 
-            console.log("Saved!");
-        });
+        console.log("Saved!");
     });
 });
